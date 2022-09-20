d8d19b44bd0415b81ec38f3eea3424e2253c52aa
Coswim <96973915+Coswim@users.noreply.github.com>
2022-09-20 21:18:30 +0900

    Update regiapp.py

--- regiapp.py
+++ regiapp.py
@@ -6,19 +6,12 @@ from pymongo import MongoClient
 import certifi
 ca = certifi.where()
 
-client = MongoClient('mongodb+srv://test:sparta@cluster0.iuqpqy6.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
+client = MongoClient('#', tlsCAFile=ca)
 db = client.dbsparta
 
 @app.route('/')
 def home():
     return render_template('index.html')
-
-
-# @app.route('/test', methods=['GET'])
-# def test_get():
-#    title_receive = request.args.get('title_give')
-#    print(title_receive)
-#    return jsonify({'result': 'success', 'msg': '이 요청은 GET!'})
 
 @app.route('/registration', methods=['POST'])
 def test_post():
@@ -39,4 +32,4 @@ def test_post():
     return jsonify({'result':'success', 'msg': 'DB 저장!'})
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5001, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
