c69d77f1f83ddde4b8eec68ec109896db601abc2
Sky-Park <alsrb7018@gmail.com>
2022-09-21 15:13:09 +0900

    html 파일에 다넣으니 복잡해서 js,css,html로 각 분리함

---
+++ 1week/app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, jsonify, request
+app = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://test:sparta@cluster0.sojhuso.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.dbsparta
+import hashlib
+
+
+@app.route('/')
+def home():
+        return render_template('membershtp.html')
+
+# [회원가입 API]
+# id, pw, nickname을 받아서, mongoDB에 저장합니다.
+# 저장하기 전에, pw를 sha256 방법(=단방향 암호화. 풀어볼 수 없음)으로 암호화해서 저장합니다.
+
+@app.route('/membership/check_dup', methods=['POST'])
+def check_dup():
+    username_receive = request.form['username_give']
+    exists = bool(db.membership.find_one({"username": username_receive}))
+    return jsonify({'result': 'success', 'exists': exists})
+
+
+@app.route('/membership/save', methods=['POST'])
+def sign_up():
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+    name_receive = request.form['name_give']
+
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+
+    doc = {
+        "username": username_receive,  # 아이디
+        "password": password_hash,     # 비밀번호
+        "name": name_receive          # 이름
+    }
+    db.membership.insert_one(doc)
+
+    return jsonify({'result': 'success'})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
